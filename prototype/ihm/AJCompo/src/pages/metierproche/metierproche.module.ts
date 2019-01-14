25033bd1cbddc54b4271c513bc66042945599770
Unknown <unknown@example.com>
2019-01-14 15:55:45 +0100

    Demo des composants utilisables

---
+++ prototype/ihm/AJCompo/src/pages/metierproche/metierproche.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MetierprochePage } from './metierproche';
+
+@NgModule({
+  declarations: [
+    MetierprochePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MetierprochePage),
+  ],
+})
+export class MetierprochePageModule {}
